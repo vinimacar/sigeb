f138023cfa4bc7160acdf19811b4e7166926cb73
vinimacar <132243328+vinimacar@users.noreply.github.com>
2025-05-22 16:24:53 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   authDomain: "sigeb-ccd3f.firebaseapp.com",
   databaseURL: "https://sigeb-ccd3f-default-rtdb.firebaseio.com",
   projectId: "sigeb-ccd3f",
-  storageBucket: "sigeb-ccd3f.firebasestorage.app",
+  storageBucket: "sigeb-ccd3f.appspot.com", // Corrigido: .appspot.com
   messagingSenderId: "209689711066",
   appId: "1:209689711066:web:5a99505e60bd34f29b70e3",
   measurementId: "G-VJBDW2B7HZ"
@@ -20,6 +20,3 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 const analytics = firebase.analytics();
-
-// Export the services
-export { auth, db, storage, analytics };
