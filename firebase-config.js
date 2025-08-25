8c29c78fa5ae7fb39c461a97529a925333d854a9
vinimacar <132243328+vinimacar@users.noreply.github.com>
2025-08-25 11:26:15 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Configuração do Firebase
-const firebaseConfig = { 
-    apiKey: "AIzaSyCkttFoV_FoPn91ClE7xFiknFvs9pXaCoQ", 
-    authDomain: "sigeb-ccd3f.firebaseapp.com", 
-    databaseURL: "https://sigeb-ccd3f-default-rtdb.firebaseio.com", 
-    projectId: "sigeb-ccd3f", 
-    storageBucket: "sigeb-ccd3f.appspot.com", 
-    messagingSenderId: "209689711066", 
-    appId: "1:209689711066:web:5a99505e60bd34f29b70e3", 
-    measurementId: "G-VJBDW2B7HZ" 
+const firebaseConfig = {
+  apiKey: "AIzaSyCkttFoV_FoPn91ClE7xFiknFvs9pXaCoQ",
+  authDomain: "sigeb-ccd3f.firebaseapp.com",
+  databaseURL: "https://sigeb-ccd3f-default-rtdb.firebaseio.com",
+  projectId: "sigeb-ccd3f",
+  storageBucket: "sigeb-ccd3f.firebasestorage.app",
+  messagingSenderId: "209689711066",
+  appId: "1:209689711066:web:c115bf3972c7541e9b70e3",
+  measurementId: "G-DTF3GZJCDZ"
 };
 
 // Inicializa o Firebase
