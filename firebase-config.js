a46c404ff6864027a69f3101450e99cf6623d87f
vinimacar <132243328+vinimacar@users.noreply.github.com>
2025-08-25 11:11:59 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,10 @@
-// Configuração do Firebase
+// Importa o Firebase
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/database';
+
+// Configuração do Firebase (você já forneceu a configuração)
 export const firebaseConfig = { 
     apiKey: "AIzaSyCkttFoV_FoPn91ClE7xFiknFvs9pXaCoQ", 
     authDomain: "sigeb-ccd3f.firebaseapp.com", 
@@ -9,3 +15,10 @@ export const firebaseConfig = {
     appId: "1:209689711066:web:5a99505e60bd34f29b70e3", 
     measurementId: "G-VJBDW2B7HZ" 
 };
+
+// Verifica se o Firebase já foi inicializado, se não, inicializa
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);  // Inicializa o Firebase com as configurações fornecidas
+} else {
+    firebase.app();  // Se já estiver inicializado, usa a instância existente
+}
